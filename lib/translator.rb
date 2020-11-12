f8403b2b352d316c7f4680d204990f27a8f2d166
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-12 02:20:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,8 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    binding.pry  
+    binding.pry
+  end 
 end
 
 def get_english_meaning(path, emoticon)
